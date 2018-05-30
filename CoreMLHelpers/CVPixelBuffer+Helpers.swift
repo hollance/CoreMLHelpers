a21b93c3ebd7ec4a3506f8993c79243049349521
Alexandr Mavrichev <alex@svntech.ru>
2018-05-30 14:15:44 +0300

    funtion for rotating CVPixelBuffer factor of 90 counterclock-wise

--- CoreMLHelpers/CVPixelBuffer+Helpers.swift
+++ CoreMLHelpers/CVPixelBuffer+Helpers.swift
@@ -124,3 +124,69 @@ public func resizePixelBuffer(_ pixelBuffer: CVPixelBuffer,
   let scaledImage = ciImage.transformed(by: scaleTransform)
   context.render(scaledImage, to: output)
 }
+
+/**
+ Rotates CVPixelBuffer by the provided factor of 90 counterclock-wise.
+ */
+
+public func rotate90PixelBuffer(_ srcPixelBuffer: CVPixelBuffer, factor: UInt8) -> CVPixelBuffer? {
+    CVPixelBufferLockBaseAddress(srcPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+    guard let srcData = CVPixelBufferGetBaseAddress(srcPixelBuffer) else {
+        print("Error: could not get pixel buffer base address")
+        return nil
+    }
+    let sourceWidth = CVPixelBufferGetWidth(srcPixelBuffer)
+    let sourceHeight = CVPixelBufferGetHeight(srcPixelBuffer)
+    var destWidth = sourceHeight
+    var destHeight = sourceWidth
+    var color = UInt8(0)
+    
+    if factor % 2 == 0 {
+        destWidth = sourceWidth
+        destHeight = sourceHeight
+    }
+    
+    let srcBytesPerRow = CVPixelBufferGetBytesPerRow(srcPixelBuffer)
+    var srcBuffer = vImage_Buffer(data: srcData,
+                                  height: vImagePixelCount(sourceHeight),
+                                  width: vImagePixelCount(sourceWidth),
+                                  rowBytes: srcBytesPerRow)
+    
+    let destBytesPerRow = destWidth*4
+    guard let destData = malloc(destHeight*destBytesPerRow) else {
+        print("Error: out of memory")
+        return nil
+    }
+    var destBuffer = vImage_Buffer(data: destData,
+                                   height: vImagePixelCount(destHeight),
+                                   width: vImagePixelCount(destWidth),
+                                   rowBytes: destBytesPerRow)
+    
+    let error = vImageRotate90_ARGB8888(&srcBuffer, &destBuffer, factor, &color, vImage_Flags(0))
+    
+    CVPixelBufferUnlockBaseAddress(srcPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+    if error != kvImageNoError {
+        print("Error:", error)
+        free(destData)
+        return nil
+    }
+    
+    let releaseCallback: CVPixelBufferReleaseBytesCallback = { _, ptr in
+        if let ptr = ptr {
+            free(UnsafeMutableRawPointer(mutating: ptr))
+        }
+    }
+    
+    let pixelFormat = CVPixelBufferGetPixelFormatType(srcPixelBuffer)
+    var dstPixelBuffer: CVPixelBuffer?
+    let status = CVPixelBufferCreateWithBytes(nil, destWidth, destHeight,
+                                              pixelFormat, destData,
+                                              destBytesPerRow, releaseCallback,
+                                              nil, nil, &dstPixelBuffer)
+    if status != kCVReturnSuccess {
+        print("Error: could not create new pixel buffer")
+        free(destData)
+        return nil
+    }
+    return dstPixelBuffer
+}
