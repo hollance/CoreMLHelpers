17f23eaa789281aa77242f2fe5a59ef3b2db57ea
araki <araki@qoncept.co.jp>
2018-11-07 10:47:41 +0900

    Add CGImage#pixelBuffer*

--- CoreMLHelpers/CGImage+CVPixelBuffer.swift
+++ CoreMLHelpers/CGImage+CVPixelBuffer.swift
@@ -28,6 +28,64 @@ import VideoToolbox
 
 extension CGImage {
     /**
+     Resizes the image to width x height and converts it to an RGB CVPixelBuffer.
+     */
+    public func pixelBuffer(width: Int, height: Int) -> CVPixelBuffer? {
+        return pixelBuffer(width: width, height: height,
+                           pixelFormatType: kCVPixelFormatType_32ARGB,
+                           colorSpace: CGColorSpaceCreateDeviceRGB(),
+                           alphaInfo: .noneSkipFirst)
+    }
+    
+    /**
+     Resizes the image to width x height and converts it to a grayscale CVPixelBuffer.
+     */
+    public func pixelBufferGray(width: Int, height: Int) -> CVPixelBuffer? {
+        return pixelBuffer(width: width, height: height,
+                           pixelFormatType: kCVPixelFormatType_OneComponent8,
+                           colorSpace: CGColorSpaceCreateDeviceGray(),
+                           alphaInfo: .none)
+    }
+    
+    func pixelBuffer(width: Int, height: Int, pixelFormatType: OSType,
+                     colorSpace: CGColorSpace, alphaInfo: CGImageAlphaInfo) -> CVPixelBuffer? {
+        var maybePixelBuffer: CVPixelBuffer?
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+                     kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue]
+        let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                         width,
+                                         height,
+                                         pixelFormatType,
+                                         attrs as CFDictionary,
+                                         &maybePixelBuffer)
+        
+        guard status == kCVReturnSuccess, let pixelBuffer = maybePixelBuffer else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer)
+        
+        guard let context = CGContext(data: pixelData,
+                                      width: width,
+                                      height: height,
+                                      bitsPerComponent: 8,
+                                      bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer),
+                                      space: colorSpace,
+                                      bitmapInfo: alphaInfo.rawValue)
+            else {
+                return nil
+        }
+        
+        context.draw(self, in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        return pixelBuffer
+    }
+}
+
+extension CGImage {
+    /**
      Creates a new CGImage from a CVPixelBuffer.
      NOTE: This only works for RGB pixel buffers, not for grayscale.
      */
