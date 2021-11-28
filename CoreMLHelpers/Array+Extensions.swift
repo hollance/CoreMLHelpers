f5de7c78d7559f57ea45e3ae493c1c69b0f25e0e
Marc Steven <MarcSteven@users.noreply.github.com>
2021-11-28 13:02:24 +0800

    Update Array+Extensions.swift

--- CoreMLHelpers/Array+Extensions.swift
+++ CoreMLHelpers/Array+Extensions.swift
@@ -22,14 +22,14 @@
 
 import Swift
 
-extension Array where Element: Comparable {
+public extension Array where Element: Comparable {
   /**
     Returns the index and value of the largest element in the array.
 
     - Note: This method is slow. For faster results, use the standalone
             version of argmax() instead.
   */
-  public func argmax() -> (Int, Element) {
+   func argmax() -> (Int, Element) {
     precondition(self.count > 0)
     var maxIndex = 0
     var maxValue = self[0]
@@ -43,14 +43,14 @@ extension Array where Element: Comparable {
   /**
     Returns the indices of the array's elements in sorted order.
   */
-  public func argsort(by areInIncreasingOrder: (Element, Element) -> Bool) -> [Array.Index] {
+  func argsort(by areInIncreasingOrder: (Element, Element) -> Bool) -> [Array.Index] {
     return indices.sorted { areInIncreasingOrder(self[$0], self[$1]) }
   }
 
   /**
     Returns a new array containing the elements at the specified indices.
   */
-  public func gather(indices: [Array.Index]) -> [Element] {
+ func gather(indices: [Array.Index]) -> [Element] {
     return indices.map { self[$0] }
   }
 }
